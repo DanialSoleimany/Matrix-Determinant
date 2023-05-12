51a015a3a6f3ad2869276d012011b05deb9ca120
Danial Soleimany <102956315+DanialSoleimany@users.noreply.github.com>
2023-05-12 11:46:28 +0330

    Matrix Determinant.py
    The determinant of a Matrix is defined as a special number that is defined only for square matrices (matrices that have the same number of rows and columns)

--- Matrix Determinant.py
+++ Matrix Determinant.py
@@ -1,41 +1,12 @@
-# Determinant of matrix 3x3
-def matrix_determinant():
- 
-    count = 0
-    row, column = 1, 1
+# Code works on square matrices (matrices that have the same number of rows and columns)
 
-    init_matrix, matrix_upgrader = {}, {count:"Blank"}
-    array = []
+import numpy as np
 
-    represent_matrix = "[a11 , a12 , a13\n a21 , a22 , a23\n a31 , a32 , a33]\n"
-    print(f"\nMatrix 3x3 is: \n{represent_matrix}")
+def determinant(matrix):
+    determinant = np.linalg.det(matrix)
+    return determinant
 
-    while count < 10:
+matrix = np.array([[5, 6],
+                   [3, 5]])
 
-        user_input = input(f"Enter a{row}{column}:\n")
-        number = float(user_input) if '.' in user_input else int(user_input)
-
-        matrix_upgrader[count] = number
-        init_matrix.update({count:number})
-
-        if column == 3:
-            column = 0
-            row += 1
-
-            if row == 4:
-                break
-
-        column += 1
-        count += 1
-
-    for index in matrix_upgrader:
-        array.append(matrix_upgrader[index])
-
-    mat = array
-    determinant = (mat[0] * mat[4] * mat[8]) + (mat[1] * mat[5] * mat[6]) + (mat[2] * mat[3] * mat[7]) - (mat[6] * mat[4] * mat[2]) - (mat[7] * mat[5] * mat[0]) - (mat[8] * mat[3] * mat[1])
-    matrix = f"[{mat[0]: ^6} , {mat[1]: ^6} , {mat[2]: ^6}\n {mat[3]: ^6} , {mat[4]: ^6} , {mat[5]: ^6}\n {mat[6]: ^6} , {mat[7]: ^6} , {mat[8]: ^6}]\n"
-
-    print(f"\nMatrix 3x3 is: \n{matrix}")
-    print(f"Determinant of matrix 3x3 is {determinant:.2f}\n")
-
-matrix_determinant()
+print("Determinant of matrix 2x2:\n{0}".format(determinant(matrix)))
